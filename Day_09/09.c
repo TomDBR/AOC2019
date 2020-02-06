016b071f77e99d1af5ebe463fb25d7685b45f881
TomDBR <canardo@cock.li>
2020-02-06 16:39:46 +0100

    DAY 09
     - fix day 09, use shell_cmd utility to call the locally installed
       intcode prg, remove the intcode C

--- Day_09/09.c
+++ Day_09/09.c
@@ -1,8 +1,26 @@
+#include <unistd.h>
 #include <stdio.h>
-#include "intcode.h"
+#include "../UTILS/shell_cmd.h"
+#include "../UTILS/util.h"
 
-int main(int argc, char *argv[]) {
-	printf("PART I: BOOST keycode: %li\n", handleInput("./input.txt", 0));
-	printf("PART II: coordinates of distress signal: %li\n", handleInput("./input.txt", 0));
+int main() {
+	long keycode = 0; FILE *f;
+	char *p1_cmd[] = { "intcode", "./input.txt", "-1", "0", NULL };
+
+	// PART 1
+	process_t p1 = process(p1_cmd);
+	dprintf(p1.fd_read, "%d\n", 1); close(p1.fd_read);
+	if (!(f = fdopen(p1.fd_write, "r"))) die("Failed to open file descriptor!");
+	fscanf(f, "%ld\n", &keycode);
+	fclose(f);
+	printf("PART I: BOOST keycode: %ld\n", keycode);
+
+	// PART 2
+	process_t p2 = process(p1_cmd);
+	dprintf(p2.fd_read, "%d\n", 2); close(p2.fd_read);
+	if (!(f = fdopen(p2.fd_write, "r"))) die("Failed to open file descriptor!");
+	fscanf(f, "%ld\n", &keycode);
+	fclose(f);
+	printf("PART II: coordinates of distress signal: %li\n", keycode);
 	return 0;
 }
