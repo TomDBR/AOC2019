df3960e639d70ba680dd3ee90e01989d8e319e04
xanadu <canardo@cock.li>
2020-06-21 16:12:25 +0200

    Attempt to automate playing day13 game, still need to figure out why it's not drawing properly, and how to improve the algorithm (if it can even be called that)

--- Day_13/day13.c
+++ Day_13/day13.c
@@ -3,9 +3,9 @@
 #include <stdlib.h>
 #include <curses.h>
 #include <unistd.h>
-#include <pthread.h>
 #include "../UTILS/shell_cmd.h"
 #include "../UTILS/util.h"
+#include <poll.h>
 
 int verbosity = 0;
 struct output **tiles = NULL;
@@ -20,19 +20,33 @@ struct output {
 	int type;
 };
 
+
+void output_to_string(struct output *tile)
+{
+	printf("TILE:\t(%d,%d) -> TYPE: %d\n", tile->x, tile->y, tile->type);
+}
+
+struct output *find_type(int type)
+{
+	for (int i = 0; i < tiles_size; i++) 
+		if (tiles[i]->type == type) return tiles[i];
+	return NULL;
+}
+
 int findTile(int x, int y)
 {
 	for (int i = 0; i < tiles_size; i++) {
 		struct output *t = tiles[i];
 		if (t->x == x && t->y == y) return i;
 	}
-	return 0;
+	return -1;
 }
 
-struct output *addTile(int x, int y, int type)
+// create new tile if it doesn't exist, otherwise update its type
+struct output *assignTile(int x, int y, int type)
 {
 	int i = 0;
-	if ((i = findTile(x, y))) {
+	if ((i = findTile(x, y)) != -1) {
 		tiles[i]->type = type;
 		return tiles[i];
 	}
@@ -70,7 +84,6 @@ int isScore(struct output *s)
 
 void redraw_screen() 
 {
-	clear();
 	int max_y = 0, max_x = 0;
 	getmaxyx(stdscr, max_y, max_x);
 	int x_start = ( max_x / 2 ) - ( width / 2 );
@@ -87,28 +100,19 @@ void redraw_screen()
 	}
 }
 
-void *update_screen(void *f) {
-	noecho();
-	curs_set(FALSE); // hide cursor
-	cbreak();
-	int x, y, type, cnt = 0;
-	int max_y = 0, max_x = 0;
-	getmaxyx(stdscr, max_y, max_x);
-	int x_start = ( max_x / 2 ) - ( width / 2 );
-	int y_start = ( max_y / 2 ) - ( height / 2 );
-	while (fscanf(f, "%d\n%d\n%d", &x, &y, &type) == 3) {
-		cnt++;
-		mvprintw(max_y - 3, 0, "in loop: [%d]. x: %d, y: %d, type: %d", cnt, x, y, type);
-		struct output *s = addTile(x, y, type);
-		if (isScore(s)) {
-			score = s->type;
-		} else {
-			char block = get_block_for_type(s->type);
-			mvprintw(y_start + y, x_start + x, &block);
-		}
-		refresh();
-	}
-	return NULL;
+int query_machine(FILE *f, int *x, int *y, int *type) {
+	int ret = fscanf(f, "%d\n%d\n%d", x, y, type);
+	if (verbosity) printf("x: %d, y: %d, type: %d\n", *x, *y, *type);
+	return ret == 3 ? 1: 0;
+}
+
+int get_move() {
+	struct output *ball = find_type(4);
+	struct output *paddle = find_type(3);
+	if (!ball || !paddle) die("something went wrong!");
+	if (paddle->x < ball->x) return 1;
+	if (paddle->x > ball->x) return -1;
+	return 0;
 }
 
 int main() 
@@ -116,15 +120,19 @@ int main()
 	signal(SIGPIPE, SIG_IGN);
 	// Run program
 	process_t machine = process(cmd);
-	FILE *f; int x, y, type;
+	// use poll to query whether there's any data to be read before using scanf
+	struct pollfd poll_f = { machine.fd_write, POLL_IN|POLL_PRI, 0 };
+	FILE *f;
+	int x, y, type; // used to save the output of the intcode program in
+	int max_y = 0, max_x = 0; // used to determine size of screen for ncurses
+
  	if (!(f = fdopen(machine.fd_write, "r"))) 
 		die("Failed to open file descriptor!\n");
 
-	while (fscanf(f, "%d\n%d\n%d", &x, &y, &type) == 3) {
-		if (verbosity) printf("x: %d, y: %d, type: %d\n", x, y, type);
+	while (query_machine(f, &x, &y, &type)) {
 		if (x == -1 && y == 0) // when this happens, the program is done drawing and starts accepting input, initial score 0
 			break;
-		addTile(x, y, type);
+		assignTile(x, y, type);
 	}
 
 	// check amount of block tiles
@@ -132,8 +140,9 @@ int main()
 	for (int i = 0; i < tiles_size; i++) {
 		if (tiles[i]->type == 2) count++;
 	}
-	printf("Amount of block tiles: %d\n", count);
+	printf("PART 1:\tAmount of block tiles: %d\n", count);
 
+	// PART 2: PLAY THE GAME
 	// Draw a screen
 	initscr();
 	noecho();
@@ -141,55 +150,34 @@ int main()
 	cbreak();
 
 	// center the screen
-	int max_y = 0, max_x = 0;
 	getmaxyx(stdscr, max_y, max_x);
-	int x_start = ( max_x / 2 ) - ( width / 2 );
-	int y_start = ( max_y / 2 ) - ( height / 2 );
-
-	// initial layout
 	redraw_screen();
 
-	//pthread_t inc_x_thread;
-	//if(pthread_create(&inc_x_thread, NULL, update_screen, f)) die("error creating thread!");
-
-
-	// start game loop
-	nodelay(stdscr, TRUE); // makes getch non blocking, returns ERR if no key pressed
-	int exit_loop = 0; // set to 1 when q is pressed to exit loop
-	while (1) {
-		if (exit_loop) break;
-		int key = getch(), res;
-		switch (key) {
-			case 108:
-				res = 1; break;
-			case 113:
-				exit_loop = 1; 
-				continue;
-			case 104:
-				res = -1; break;
-			default:
-				res = 0;
-		}
-		mvprintw(max_y - 1, 0, "key: %c\t(digit: %d) -> res: %d", (char) key, key, res);
+	while((waitpid(machine.pid, NULL, WNOHANG)) == 0) {
+		int res = get_move(), score = 0;
+		mvprintw(max_y - 1, 0, "MOVING TO THE %s!", abs(res) == 1 ? res == 1 ? "RIGHT": "LEFT" : "NOT");
 		mvprintw(max_y - 2, 0, "Current score: %d", score);
 		dprintf(machine.fd_read, "%d\n", res);
-		int x, y, type;
 		for (int i = 0; i < (abs(res) ? 4 : 2); i++) {
-			fscanf(f, "%d\n%d\n%d", &x, &y, &type);
-			mvprintw(max_y - 3, 0, "in loop: [%d]. x: %d, y: %d, type: %d", i, x, y, type);
-			struct output *s = addTile(x, y, type);
-			if (isScore(s)) {
-				score = s->type;
+			query_machine(f, &x, &y, &type);
+			assignTile(x, y, type);
+		}
+		if (poll(&poll_f, 1, 100)) {
+			query_machine(f, &x, &y, &type);
+			switch (type) {
+				case 0:
+					//printf("SCORE: %d, YOU DIED!\n", type);
+					mvprintw(max_y - 3, 0, "SCORE: %d, YOU DIED!\n", type);
+					break;
+				default:
+					score = type;
+					mvprintw(max_y - 3, 0, "SCORE: %d\n", score);
 			}
-			//} else {
-			//	char block = get_block_for_type(s->type);
-			//	mvprintw(y_start + y, x_start + x, &block);
-			//}
 		}
 		redraw_screen();
-		refresh();
-		usleep(200000);
+		usleep(10000);
 	}
+	usleep(1000000);
 	clear();
 	endwin();
 	wait(NULL);
