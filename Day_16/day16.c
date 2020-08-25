3ff7714517a4b32032123efa4bc1f04d24725c7f
TomDBR <canardo@cock.li>
2020-08-25 22:32:44 +0200

    day16 part1 done

--- Day_16/day16.c
+++ Day_16/day16.c
@@ -13,30 +13,32 @@ int *getPattern(int *base, int mul)
 	}
 	return pattern;
 }
+int between(int val, int min, int max) {
+	return val <= max && val >= min;
+}
+
 int main() {
 	FILE *f = fopen("./input.txt", "r");
-	char c;
+	char c[2] = { ' ', '\0' };
 	int *phase = NULL, *newPhase = NULL, phaseLen = 0;
 	int basePattern[4] = { 0,1,0,-1 };
-	while ((c = fgetc(f)) != EOF) {
+	while ((c[0] = fgetc(f)) != EOF) {
+		if (!between((int) c[0], (int) '0', (int) '9')) continue;
 		phase = realloc(phase, ++phaseLen * sizeof(int));
-		phase[phaseLen-1] = atoi(&c);
+		phase[phaseLen-1] = atoi(c);
 	}
-	newPhase = malloc(phaseLen * sizeof(int));
+	newPhase = calloc(phaseLen, sizeof(int));
 	fclose(f);
 
 	for (int i = 0; i < 100; i++) {
 		for (int j = 0; j < phaseLen; j++) {
 			int *pattern = getPattern(basePattern,j+1);
 			int patternIdx = 1, sizePattern = (j+1) * 4;
-			printf("sizeof pattern: %d\n", sizePattern);
 			int sum = 0;
 			for (int idx = 0; idx < phaseLen; idx++) {
-				//printf("%d*%d + ", pattern[patternIdx], phase[idx]);
 				sum += (pattern[patternIdx] * phase[idx]);
 				patternIdx = patternIdx == sizePattern - 1 ? 0 : patternIdx+1;
 			}
-			//printf(" = sum is: %d => %d\n", sum, abs(sum) % 10);
 			newPhase[j] = abs(sum) % 10;
 			free(pattern);
 		}
@@ -44,10 +46,9 @@ int main() {
 			phase[idx] = newPhase[idx];
 	}
 	printf("after %d phases: ", 100);
-	for (int idx = 0; idx < 8; idx++) {
-		printf("%d", phase[idx]);
-	}
+	for (int idx = 0; idx < 8; idx++) printf("%d", phase[idx]);
 	printf("\n");
+
 	free(phase); free(newPhase);
 	return 0;
 }
